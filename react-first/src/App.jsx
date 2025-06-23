d3178879636c63360110f54cf57ed74af51522fa
sejin <tp028@naver.com>
2025-06-24 03:03:51 +0900

    mac

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    let [count, setCount] = useState('남자 코트 추천');
+    // useState 실행 결과 [a, b] 두 개의 배열이 결과로 발생
+    // a에는 남자 코트 추천이 그대로 들어감
+    // b에서는 값을 수정해주기 위한 함수가 들어감
+    // 웹이 앱처럼 동작하게 만들기 위해 자주 변경되는 값을 state로 사용한다. 
+    // => state를 사용하면 HTML이 자동으로 다시 랜더링된다. 
+
+    return(
+        <p> { count } </p>
+    );
 }
 
-export default App
+export default App;
--- react-first/src/main.jsx
+++ react-first/src/main.jsx
@@ -1,10 +1,55 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// 리액트 파일 중에서 가장 먼저 실행되는 파일 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { Fragment } from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App';
+
+
+
+// react-dom 패키지에서 디폴트로 익스포트 하는 객체를 ReactDom 이라는 이름으로 임포트해서 
+// 이 객체의 render 메소드를 실행한다. 
+const root = ReactDom.createRoot(document.getElementById('root'));
+/*
+root.render(
+    <Fragment> 
+        <p>안녕</p>
+        <p>리액트</p>
+    </Fragment>
+);
+*/
+// render 메소드를 이용해서 html 태그를 만들어 준다.
+// 첫 번째 아규먼트로 html 태그를 사용
+// Dom 요소들 중 root라는 id를 가지고 있는 html 요소가 두 번째 아규먼트로 사용
+// render 메소드가 실행되면 첫 번째 아규먼트로 새로운 html요소를 만들고 만든 요소를 두 번째 아규먼트 값으로 넣는다.
+
+// fragment를 사용하면 불필요한 <div> 사용을 줄일 수 있다 축약해서 <>로 사용할 수 있다.
+
+// 자바스크립트의 변수를 사용하는 방법 -> 중괄호를 이용한다.
+const product = 'MacBook';
+
+const imageUrl =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png';
+
+
+function handleClick() {
+  alert('곧 도착합니다!');
+}
+
+/*root.render(
+  <h1>나만의 {product} 주문하기</h1>
+);
+*/
+
+
+// 괄호 안에는 자바스크립트로 된 표현식을 사용할 수 있다. 
+root.render(
+  <>
+  <App/>
+    <h1>나만의 {product.toUpperCase()} 주문학기</h1>
+    <img src={imageUrl} alt="제품 사진"/>
+    <button onClick={handleClick}>확인</button>
+  </>
+);
+// 괄호 안에서 자바스크립트의 메소드를 사용할 수 있다. 
+
+// 데이터 바인딩을 쉽게 할 수 있음!
