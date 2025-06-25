0cd5c35b27aee0ecd4ff23a43fe1f4e23799c4c2
sejin <tp028@naver.com>
2025-06-25 22:00:26 +0900

    css

--- rockpaper/src/HandButton.jsx
+++ rockpaper/src/HandButton.jsx
@@ -1,25 +1,12 @@
 import HandIcon from "./HandIcon";
-
-const style = {
-  width: '166px',
-  height: '166px',
-  border: 'none',
-  outline: 'none',
-  textAlign: 'center',
-  cursor: 'pointer',
-  backgroundColor: 'transparent',
-  backgroundImage: `url('/purple.svg')`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  backgroundSize: 'contain',
-};
+import './HandButton.css';
 
 
 function HandButton({ value, onClick}) {
     const HandleClick = () => onClick(value);
     return(
-        <button style={style} onClick={HandleClick}>
-        <HandIcon value = {value} /> 
+        <button className='HandButton ' onClick={HandleClick}>
+        <HandIcon className='HandButton-icon' value = {value} /> 
         </button>
     )
 }
